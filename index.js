f0f66a7923005287736672fb7c28f3a39a2f5377
Anand Janardhanan <ajkadakkal@gmail.com>
2026-05-18 23:38:46 +0530

    CRUD operations

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
 const express = require("express");
-const app = express();
+const app=express();
+
 app.use(express.json());
 
- function index(req,res) {
-  return res.send("Hi World !!!"); 
-}
-app.get("/",index);
-app.get("/users/:id",(req,res)=> {
-  const {id} = req.params;
-  console.log(id);
+let users=[{id:1,name:"sam",email:"sam@gmial.com"},
+{id:2,name:"ken",email:"ken@gmial.com"},{id:3,name:"tom",email:"tomm@gmial.com"},
+];
+
+app.get("/", (req,res)=> res.send(users));
+app.get("/user/:id", (req,res)=>{
+const {id} = req.params;
+return res.json(users.find((user)=> user.id==id));
+});
+
+app.post("/",( req, res)=> {
+  const {name, email}= req.body;
+  const id =users.length+1;
+  const user={id,name,email};
+  users.push(user);
+  return res.status(201).json(user);
+})
+
+app.delete("/user/:id", (req,res)=>{
+const {id} = req.params;
+const user=users.find((user)=> user.id==id);
+if(!user)
+  return res.status(404).send();
+
+users=users.filter((user)=> user.id !=id);
+return res.status(204).send(); 
+});
+
+app.patch("/user/:id", (req,res)=>{
+const {id} = req.params;
+const user=users.find((user)=> user.id==id);
+if(!user)
+  return res.status(404).send();
+const {name, email}= req.body;
+if(name)
+  user.name=name;
+if(email)
+  user.email=email;
+const index=users.findIndex((user)=> user.id==id);
+users[index]=user;
+return res.json(user); 
+});
+
+
+
+app.listen(3000,"0.0.0.0",()=>{
+  console.log("Server is running on port 3000");
   
 });
-app.post("/",(req,res)=> {
-console.log(req.body);
- return res.send("rec added")}
-);
-
-app.delete("/",(req,res)=> {
-  return res.send("delete");
-});
-
-app.listen(3000,'0.0.0.0',()=>{
-  console.log("Server is running on port 3000");
-})
