7eda34733d04217ce0126bed690656a0d73d0ac6
Anand Janardhanan <ajkadakkal@gmail.com>
2026-05-18 18:49:00 +0530

    simple route

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(req,res)=> {res.send('Hello World')});
+app.get("/",(req,res)=> {res.send('Hello World+++!!!')});
+app.get("/users",(req,res)=> {res.send('users')});
+app.get("/users/:id",(req,res)=> {res.send('users any')});
+app.get("/users/:id/create",(req,res)=> {res.send('users any create form')});
 
 app.listen(3000,'0.0.0.0',()=>{
   console.log("Server is running on port 3000");
